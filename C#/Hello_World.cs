517b0d6dd774dcd79c88b3ee386d07581e33b48a
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-19 16:08:25 +0100

    Added first problem

---
+++ C#/Hello_World.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    /*Output should contain one line, containing the string “Hello World!”*/
+
+    class Hello_World
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }   
+    }
+}
