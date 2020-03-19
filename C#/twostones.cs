6d4d21201fbe9c00d2b4edf66bab8d92da0207f5
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-19 16:16:32 +0100

    Added twostones

---
+++ C#/twostones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KattisProblems
+{
+    class twostones
+    {
+        static void Main(string[] args)
+        {
+            int stone = int.Parse(Console.ReadLine());
+            if (stone %2 == 0)
+            {
+                Console.WriteLine("Bob");
+            }
+            else
+            {
+                Console.WriteLine("Alice");
+            }
+        }
+    }
+}
