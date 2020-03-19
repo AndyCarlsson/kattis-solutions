4697814d78ca4f7748bebd26ab47f946157b760e
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-19 16:27:54 +0100

    1.2 twostones

--- C#/twostones.cs
+++ C#/twostones.cs
@@ -1,16 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KattisProblems
 {
     class twostones
     {
+
+        /* Alice and Bob in turns take exactly two consecutive stones on the ground until 
+         * there are no consecutive stones on the ground. 
+         * That is, each player can take stone i and stone i+1, where 1≤i≤N−1. 
+         * If the number of stone left is odd, Alice wins. Otherwise, Bob wins.*/
+
         static void Main(string[] args)
         {
             int stone = int.Parse(Console.ReadLine());
+
             if (stone %2 == 0)
             {
                 Console.WriteLine("Bob");
