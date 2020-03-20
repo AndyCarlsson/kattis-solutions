7b746920e901313e7c891e05a32dac74607c3e29
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-20 14:27:00 +0100

    1.3 r2

---
+++ C#/r2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class r2
+    {
+
+        /* The number S is called the mean of two numbers R1 and R2 if S is equal to 
+         * (R1+R2)/2. Mirko’s birthday present for Slavko was two integers R1 and R2.
+         * Slavko promptly calculated their mean which also happened to be an integer 
+         * but then lost R2! Help Slavko restore R2. */
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] firstValues = input.Split(' ');
+
+            int num1 = int.Parse(firstValues[0]);
+            int num2 = int.Parse(firstValues[1]);
+
+            int output = num2 * 2 - num1;
+
+            Console.WriteLine(output);
+        }
+    }
+}
