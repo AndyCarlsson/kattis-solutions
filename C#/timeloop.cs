985f5f781ff871b241f3edb8d2d45c22de38017f
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-20 09:02:38 +0100

    1.2 timeloop

---
+++ C#/timeloop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class timeloop
+    {
+        /* The wizard gives you a magic number, and you must count up to that number, starting at 1, saying “Abracadabra” each time.
+         Output the entire wizard’s spell by counting from 1 to N, giving one number and “Abracadabra” per line.*/
+
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= input; i++)
+            {
+                Console.WriteLine($"{i} Abracadabra");
+            }
+        }
+    }
+}
