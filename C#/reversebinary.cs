4af77cdfa44d69f745962e59f7a9eb9a557d7584
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-25 15:20:11 +0100

    1.5 reversebinary

---
+++ C#/reversebinary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace KattisProblems
+{
+    class reversebinary
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            string binaryString = "";
+            
+            double binaryNum = 0;
+            while (inputNum != 0)
+            {
+                if (inputNum % 2 == 0)
+                    binaryString += "0";
+                else if (inputNum % 2 == 1)
+                    binaryString += "1";
+                inputNum /= 2;
+            }
+
+            int[] binaryIntArr = new int[binaryString.Length];
+
+            for (int i = 0; i < binaryString.Length; i++)
+            {
+                binaryIntArr[i] = int.Parse(binaryString[i].ToString());
+            }
+            for (int i = 0; i < binaryIntArr.Length; i++)
+            {
+                if (binaryIntArr[i] == 1)
+                {
+                    binaryNum += 1 * Math.Pow(2, binaryIntArr.Length - i-1);
+                }
+            }
+            Console.WriteLine(binaryNum);
+            Console.ReadLine();
+        }
+    }
+}
